aad3087ec4774db64dcaac34bef7f63378ebb402
Brandon Keepers <brandon@opensoul.org>
2012-08-14 10:37:07 -0700

    Use Rails logger for OmniAuth logging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_OAUTH_KEY'], ENV['GITHUB_OAUTH_SECRET']
 end
+
+OmniAuth.config.logger = Rails.logger
