d9990aa75e2ab02cb22de7318c2ab14e337bc968
Brandon Keepers <brandon@opensoul.org>
2012-08-15 23:15:12 -0700

    get database name from mongo uri

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,5 @@
-url  = ENV["GH_MONGODB_URL"] || ENV["MONGODB_URI"] || ENV["MONGOHQ_URL"]
-conn = Mongo::Connection.from_uri(url || "mongodb://localhost:27017")
+uri  = URI.parse ENV["GH_MONGODB_URL"] || ENV["MONGODB_URI"] || ENV["MONGOHQ_URL"]
+conn = Mongo::Connection.from_uri(uri.to_s)
+db   = conn.db(uri.path.blank? ? "swordfish-#{Rails.env}" : uri.path.sub('/', ''))
 
-Swordfish::Application.config.mongo = conn.db("vault-#{Rails.env}")
+Swordfish::Application.config.mongo = db
