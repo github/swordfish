da7c7593583b14ab24312d54ae3ab85c4f4b4580
Brandon Keepers <brandon@opensoul.org>
2012-08-28 17:58:46 -0400

    Kill remnants of session

--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < ApplicationController
-  skip_before_filter :sign_in_required
-
-  def destroy
-    self.current_user = nil
-    redirect_to root_path
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Swordfish::Application.routes.draw do
     resource :rsa, :only => [:create, :update], :controller => 'rsa'
   end
 
-  match '/signout' => 'sessions#destroy', :as => :sign_out
-
   match 'mockup', :to => 'dashboard#mockup'
   root :to => 'dashboard#index'
 end
